0cfd3934d0f40e5d746809a5d3ab8b9ab1700e93
TheB05 <branderbaez02@gmail.com>
2024-10-03 18:59:10 -0400

    Created "Students.cs" model.

--- Models/Students.cs
+++ Models/Students.cs
@@ -1,4 +1,18 @@
 using System;
 using System.ComponentModel.DataAnnotations;
 
-class Student{}
+class Student{
+    public String id { get; set; } = string.Empty;
+
+    public string Name { get; set; } = "";
+
+    public string Lastname { get; set; } = "";
+
+    public string Grade { get; set; } = "";
+
+    public string ClassID { get; set; } = "";
+
+    public DateTime Date { get; set; } = DateTime.Now;
+
+    public string Motive { get; set; } = "";
+}
